ae3c10f5e0e9bd768820dad83d6f552d81b5e26f
waveshare <36142105+waveshare@users.noreply.github.com>
2023-10-24 12:24:11 -0700

    first working prototype

--- Python Examples/test.py
+++ Python Examples/test.py
@@ -1,4 +1,17 @@
 from nvidia_racecar import NvidiaRacecar
-
+from time import sleep
 car = NvidiaRacecar()
-car.steering = 0.3
+
+for i in range(10):
+    car.steering += 0.1
+    sleep(0.2)
+for i in range(20):
+    car.steering -= 0.1
+    sleep(0.2)
+for i in range(10):
+    car.steering += 0.1
+    sleep(0.2)
+
+car.throttle = 1
+sleep(5)
+car.throttle = 0
